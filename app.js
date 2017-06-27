b6d79a7a95dcdad03889824ead34c1af49c6ea26
Paul Bland <paul.bland@gmail.com>
2017-06-26 22:51:06 -0400

    pushing latest version live

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 var express 	= require('express');
 var app 		= express();
 var fs 			= require('fs');
-var path    	= require("path");
+var path    	= require('path');
+
+var mongoose 	= require('mongoose');
 
 
+
+
+// DATABASE
+
+// Set up default mongoose connection
+mongoose.connect(process.env.MONGODB_URI);
+
+// Get the default connection
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Define a schema
+var Schema = mongoose.Schema;
+
+var PriceRecordSchema = new Schema({
+    datetime	: Date,
+    value_sell 	: Number,
+    value_buy 	: Number
+});
+
+// Compile model from schema
+var PriceRecordModel = mongoose.model('PriceRecordModelBTC', PriceRecordSchema);
+
+
+
+
+
+
+
+// Variables accessed globally defined here
 var total_coins_owned;
 var buy_sell_unit;
 var total_spent;
@@ -11,26 +46,51 @@ var total_sold;
 var total_transactions;
 var max_coins_ever_owned;
 var max_value_ever_owned;
+var real_db_data;
 
 
 
+// INDEX - nothing there
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname+'/index.html'));
 })
 
-app.get('/sim-run-once', function(req, res) {
-	var result = testLoop();
-	res.send(result);
-})
+// THE OLD ONE - without db connection
+// app.get('/sim-run-once', function(req, res) {
+// 	var result = testLoop();
+// 	res.send(result);
+// })
 
+// Not written yet
 app.get('/sim-run-multiple', function(req, res) {
 	var result = simRunMultiple();
 	res.send(result);
 })
 
-app.get('/create', function(req, res) {
-	createRandomData();
+
+// This is the one! 
+app.get('/sim-run-once', function(req, res) {
+
+	PriceRecordModel.find({}, function(error, data){
+   		if (error) {
+            res.json(error);
+        }
+        else {
+            // copy retund db object into dat object for texst script
+    		real_db_data = data;
+
+    		// run the test! and send result to brwoser
+    		var result = testLoop();
+			res.send(result);
+        }
+	});
 })
+
+
+// for creating random data. wont need this anymore probably
+// app.get('/create', function(req, res) {
+// 	createRandomData();
+// })
 
 app.listen(process.env.PORT, function() { 
 	console.log('running on port: ' + process.env.PORT)
@@ -39,17 +99,19 @@ app.listen(process.env.PORT, function() {
 
 
 
-
-
+/*
+ * this function sets up all parameters and runs the decideSellOrBuy fn for each period
+ */
 function testLoop() {
-
 
 	var result = '';
 
 	var interval_in_minutes = 10	// how often data is collected in minutes
-	var hrs_in_period 		= 24	// working on full days = 24
+	var hrs_in_period 		= 13; 	// working on full days = 24
 
-	var btc_data 			= require('./data/btc_data')	// 30 days of data (144*30 = 4320)
+	//var btc_data 			= require('./data/btc_data')	// 30 days of data (144*30 = 4320)
+	var btc_data 			= real_db_data	// real!
+	
 	var values_per_period 	= ((hrs_in_period * 60) / interval_in_minutes); // 144; // there are 144 10-min incremetns in a day (24 hrs period)
 	
 	total_coins_owned 		= 0;
@@ -60,20 +122,21 @@ function testLoop() {
 	max_coins_ever_owned 	= 0;
 	max_value_ever_owned 	= 0;
 
-
 	var days_in_records = ((btc_data.length/24/60)*interval_in_minutes);
 
+	result += 'analyzing: ' + btc_data.length + ' values ('+days_in_records.toFixed(2)+' days)<br /><br />';
 
 	for (i=0; i<=(btc_data.length - values_per_period); i++) {
 		
 		// define start and end indexes for main array
-		result += '<strong><u>Period ' + Math.floor((i+values_per_period)/values_per_period) + ' of ' + (btc_data.length/values_per_period) + ' ';
+		result += '<strong><u>Period ' + Math.floor((i+values_per_period)/values_per_period) + ' of ' + (btc_data.length/values_per_period).toFixed(2) + ' ';
 		result += '(in '+hrs_in_period+' hr periods)</u></strong> ';
-		result += '(' + days_in_records + ' days) ';
+		result += '(' + days_in_records.toFixed(2) + ' days) ';
 		result += '(increment ' + ((i % values_per_period)+1) + ' of ' + values_per_period + ') ';
 		result += 'testing slice: ' + i + ' --> ' + (i+values_per_period) + '<br />';
 
 		// get 24 hrs worth of data (As a slice of 144 values)
+		// actually not just 24 hrs anymore. "a period" of 24 hrs )For example) is 144 values
 		var data_to_be_tested = btc_data.slice(i, (i+values_per_period));
 
 		// run the decide algorithm on just this part
@@ -81,22 +144,23 @@ function testLoop() {
 	}
 
 	return result;
-
-
 }	
 
 
 
+
+/* 
+ * this function takes a slide of the array (144 values for a day, less for other periods) and decides on selling or buying
+ */
 function decideBuyOrSell(data_to_be_tested) {
 
-	var result = '';
+	var result				= '';
 	var avg_for_period		= 0;	// orig was 24 hrs 'avg_for_24_hrs'
 	var latest_buy_price	= 0;	// this will be the currect price we're evaluating
 	var latest_sell_price	= 0;	// this will be the currect price we're evaluating
 
-	var high_threshold  	= 0.08;	// as a %
-	var low_threshold 		= 0.08;	// as a %
-
+	var high_threshold  	= 0.04;	// as a %
+	var low_threshold 		= 0.03;	// as a %
 
 	// print length to browser to make sure correct
 	//result += 'length is: ' + data_to_be_tested.length + '<br>';
@@ -111,9 +175,9 @@ function decideBuyOrSell(data_to_be_tested) {
 	// print avg result to browser
 	result += 'average price for last 24 hrs is: $' + avg_for_period + '<br>';
 
-	//get latest price
-	latest_buy_price = data_to_be_tested[data_to_be_tested.length-1].value_buy;
-	latest_sell_price = data_to_be_tested[data_to_be_tested.length-1].value_sell;
+	// get latest prices
+	latest_buy_price 	= data_to_be_tested[data_to_be_tested.length-1].value_buy;
+	latest_sell_price 	= data_to_be_tested[data_to_be_tested.length-1].value_sell;
 
 	// print latest to browser
 	result += 'latest buy price: $' + latest_buy_price.toFixed(2) + '<br>';
@@ -128,16 +192,15 @@ function decideBuyOrSell(data_to_be_tested) {
 
 	
 	if (latest_sell_price > avg_plus_high_threshold) {
-		// check if price is higher than 24 hr avg + threshold
-		result += 'latest price is higher than +' + high_threshold + '% --- sell!<br />';
 
+		// check if price is higher than period avg + threshold
+		result += 'latest price is higher than +' + high_threshold + '% --- sell!<br />';
 		result +=  sellCoin(latest_sell_price)
 
 	} else if (latest_buy_price < avg_minus_low_threshold) {
 
-		// check if price is lwoer than 24 hr avg + threshold
+		// check if price is lwoer than period avg - threshold
 		result += 'latest price is lower than -' + high_threshold + '% --- buy!<br />';
-
 		result +=  buyCoin(latest_buy_price)
 
 	} else {
@@ -147,7 +210,6 @@ function decideBuyOrSell(data_to_be_tested) {
 	result += printCurrentPosition(latest_buy_price, latest_sell_price);
 
 	return result;
-
 }
 
 
@@ -156,6 +218,7 @@ function decideBuyOrSell(data_to_be_tested) {
 
 function buyCoin(current_coin_price_buy) {
 
+	// i think this line will mean you only make one purchase at a time
 	//if (total_coins_owned > 0) return false;
 
 	var result 					= '';
@@ -165,8 +228,10 @@ function buyCoin(current_coin_price_buy) {
 	total_spent 				+= buy_sell_unit;
 	total_transactions++;
 
+	// update value for max coins ever owned
 	max_coins_ever_owned = (total_coins_owned > max_coins_ever_owned) ? total_coins_owned : max_coins_ever_owned;
 
+	// update value for max value of coins ever owned
 	var value_of_coins_owned_right_now = (total_coins_owned * current_coin_price_buy)
 	max_value_ever_owned = (value_of_coins_owned_right_now > max_value_ever_owned) ? value_of_coins_owned_right_now : max_value_ever_owned;
 
@@ -176,10 +241,11 @@ function buyCoin(current_coin_price_buy) {
 } 
 
 
+
 function sellCoin(current_coin_price_sell) {
 
 	if (total_coins_owned === 0) {
-		return 'dont have any coins to sell! returning<br/>';
+		return 'Don’t have any coins to sell! returning<br/>';
 	}
 
 	// SELL EVERYTHING
@@ -188,7 +254,6 @@ function sellCoin(current_coin_price_sell) {
 	var result_of_this_sale = (current_coin_price_sell*total_coins_owned)
 	total_sold 				+= result_of_this_sale;
 	total_transactions++;
-
 
 	result += '<span style="color:red">TRANSACTION: SELL ALL COINS: ' +  total_coins_owned + ' BTC valued at $' + current_coin_price_sell + ' = $' + result_of_this_sale + '</span><br />';
 
@@ -204,19 +269,21 @@ function printCurrentPosition(current_coin_price_buy, current_coin_price_sell) {
 
 	var result = '<strong> &gt;&gt; CURRENT POSITION</strong><br />';
 
-	result += '&gt; total bitcoins owned right now: ' + total_coins_owned + '<br/>';
-	result += '&gt; current bitcoin sell price: $' + current_coin_price_sell.toFixed(2) + '<br/>';
-	result += '&gt; total coins owned value (as sell price) = $' + (total_coins_owned * current_coin_price_sell).toFixed(2) + '<br />';
-	result += '&gt; total money invested spent = $' + total_spent + '<br />';
-	result += '&gt; total sold   = $' + total_sold.toFixed(2) + '<br />';
-	result += '&gt; total position (coins+total sold-investments): $' + ((total_coins_owned * current_coin_price_sell) + total_sold - total_spent).toFixed(2) + '<br />';
-	result += '&gt; total transaction ' + total_transactions + '<br />';
-	result += '&gt; max coins ever owned ' + max_coins_ever_owned + '<br />';
-	result += '&gt; max value ever owned $' + max_value_ever_owned.toFixed(2) + '<br /><br />';
+	result += '&gt; total bitcoins owned right now: ' 					+ total_coins_owned + '<br/>';
+	result += '&gt; current bitcoin sell price: $' 						+ current_coin_price_sell.toFixed(2) + '<br/>';
+	result += '&gt; total coins owned value (as sell price) = $' 		+ (total_coins_owned * current_coin_price_sell).toFixed(2) + '<br />';
+	result += '&gt; total money invested spent = $' 					+ total_spent + '<br />';
+	result += '&gt; total sold = $' 									+ total_sold.toFixed(2) + '<br />';
+	result += '&gt; total position (coins+total sold-investments): $' 	+ ((total_coins_owned * current_coin_price_sell) + total_sold - total_spent).toFixed(2) + '<br />';
+	result += '&gt; total transactions ' 								+ total_transactions + '<br />';
+	result += '&gt; max coins ever owned ' 								+ max_coins_ever_owned + '<br />';
+	result += '&gt; max value ever owned $' 							+ max_value_ever_owned.toFixed(2) + '<br /><br />';
 
 	return result;
 
 }
+
+
 
 
 
