861e226899ce34cbafbcf8267732ff68354ff10c
Paul Bland <paul.bland@gmail.com>
2017-07-02 09:23:04 -0400

    remove one more value

--- controllers/simulation.js
+++ controllers/simulation.js
@@ -62,7 +62,7 @@ module.exports = {
 			// SHORT DATA FOR HEROKU 30 SEC TIMEOUT (~28 SEC)
 			var periods 		= [12, 24];
 			var offsets 		= [0, 12, 24];
-			var low_values 		= [0.010, 0.025, 0.050, 0.075, 0.100, 0.125];
+			var low_values 		= [0.025, 0.050, 0.075, 0.100, 0.125];
 			var high_values 	= [0.01, 0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70];
 
 		} else if (this.buy_sell_method === 'peak') {
