6fa39c8ed878161cbb84991b0e1cbe3643858213
Paul Bland <paul.bland@gmail.com>
2017-07-18 08:42:30 -0400

    added new test result to look at at work

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -35,15 +35,15 @@ module.exports = {
 	 * ......
 	 */
 	period_offset_combos: function() {
-		return [
+		//return [
 			// {period: 7, offset: 13.5},
 		 	// {period: 7, offset: 14},
-			 {period: 9, offset: 12},    //<--- best value is in here! $1355
+			// {period: 9, offset: 12},    //<--- best value is in here! $1355
 			// {period: 6, offset: 13},
 			// {period: 8, offset: 11.5},
 			// {period: 9, offset: 11.5},
 			// {period: 4, offset: 17}
-		];
+		//];
 		
 		var res = [];
 		var set = [18.5, 19, 19.5, 20, 20.5, 21, 21.5]	// [19, 19.5, 20, 20.5, 21], [19, 20, 21], [21]
