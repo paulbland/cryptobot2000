4aac4bce46e19f69b46288f9a8ce1065822a1fd3
Paul Bland <paul.bland@gmail.com>
2017-07-24 15:04:36 -0400

    trying a different module

--- clock.js
+++ clock.js
@@ -1,11 +1,16 @@
-var CronJob         = require('cron').CronJob;
-//var gdaxPriceBot    = require('./bots/gdax-price-bot.js')
+// var CronJob         = require('cron').CronJob;
+// //var gdaxPriceBot    = require('./bots/gdax-price-bot.js')
 
-// every 10 mins
-//new CronJob('*/10 * * * *', function() {
-new CronJob('*/5 * * * * *', function() {
-    console.log('works here');
-  //gdaxPriceBot.run()
-}, null, true, 'America/New_York');
+// // every 10 mins
+// //new CronJob('*/10 * * * *', function() {
+// new CronJob('*/5 * * * * *', function() {
+//     console.log('works here');
+//   //gdaxPriceBot.run()
+// }, null, true, 'America/New_York');
 
 
+var schedule = require('node-schedule');
+ 
+var j = schedule.scheduleJob('*/5 * * * * *', function(){
+  console.log('The answer to life, the universe, and everything!');
+});
