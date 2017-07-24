6a8f0ac3561b5636d8d16760b346d7754ad8a4ba
Paul Bland <paul.bland@gmail.com>
2017-07-24 15:02:17 -0400

    try just console

--- clock.js
+++ clock.js
@@ -1,11 +1,11 @@
 var CronJob         = require('cron').CronJob;
-var gdaxPriceBot    = require('./bots/gdax-price-bot.js')
+//var gdaxPriceBot    = require('./bots/gdax-price-bot.js')
 
 // every 10 mins
 //new CronJob('*/10 * * * *', function() {
 new CronJob('*/5 * * * * *', function() {
     console.log('works here');
-  gdaxPriceBot.run()
+  //gdaxPriceBot.run()
 }, null, true, 'America/New_York');
 
 
