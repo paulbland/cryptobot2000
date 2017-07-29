6607f1c9a7259984fcbd032a3cd7dc9e5d78dc5a
Paul Bland <paul.bland@gmail.com>
2017-07-29 14:26:32 -0400

    made test vars +/- 2 units instead of one. since things are so much faster now

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -1,29 +1,29 @@
 module.exports = {
 	
-	// EVERYTHING FROM SIM VARs +/- 1 units (0.5 or 0.005) 
+	// EVERYTHING FROM SIM VARs +/- 2 units (0.5 or 0.005) 
 	low_values : {
-		'15_days' 	: genValues(0.160, 0.175, 0.005, 3),
-		'30_days' 	: genValues(0.130, 0.145, 0.005, 3),
-		'45_days' 	: genValues(0.135, 0.165, 0.005, 3),
-		'60_days' 	: genValues(0.135, 0.165, 0.005, 3),
-		'75_days' 	: genValues(0.135, 0.160, 0.005, 3),
-		'90_days' 	: genValues(0.075, 0.090, 0.005, 3)
+		'15_days' 	: genValues(0.155, 0.180, 0.005, 3),
+		'30_days' 	: genValues(0.125, 0.150, 0.005, 3),
+		'45_days' 	: genValues(0.130, 0.170, 0.005, 3),
+		'60_days' 	: genValues(0.130, 0.170, 0.005, 3),
+		'75_days' 	: genValues(0.130, 0.165, 0.005, 3),
+		'90_days' 	: genValues(0.070, 0.095, 0.005, 3)
 	},
 	high_values : {
-		'15_days' 	: genValues(0.185, 0.230, 0.005, 3), 
-		'30_days' 	: genValues(0.175, 0.200, 0.005, 3), 
-		'45_days' 	: genValues(0.180, 0.200, 0.005, 3), 
-		'60_days' 	: genValues(0.180, 0.200, 0.005, 3), 
-		'75_days' 	: genValues(0.145, 0.175, 0.005, 3), 
-		'90_days' 	: genValues(0.145, 0.175, 0.005, 3)
+		'15_days' 	: genValues(0.180, 0.235, 0.005, 3), 
+		'30_days' 	: genValues(0.170, 0.205, 0.005, 3), 
+		'45_days' 	: genValues(0.175, 0.205, 0.005, 3), 
+		'60_days' 	: genValues(0.175, 0.205, 0.005, 3), 
+		'75_days' 	: genValues(0.140, 0.180, 0.005, 3), 
+		'90_days' 	: genValues(0.140, 0.180, 0.005, 3)
 	},
 	period_offset : {
-		'15_days' 	: genCombos(14.5, 17, 0.5, 1),
-		'30_days' 	: genCombos(16, 19, 0.5, 1),
-		'45_days' 	: genCombos(16, 18.5, 0.5, 1), 
-		'60_days' 	: genCombos(16, 18.5, 0.5, 1), 
-		'75_days' 	: genCombos(12, 14.5, 0.5, 1),
-		'90_days' 	: genCombos(11.5, 14.5, 0.5, 1)
+		'15_days' 	: genCombos(14, 17.5, 0.5, 1),
+		'30_days' 	: genCombos(15.5, 19.5, 0.5, 1),
+		'45_days' 	: genCombos(15.5, 19, 0.5, 1), 
+		'60_days' 	: genCombos(15.5, 19, 0.5, 1), 
+		'75_days' 	: genCombos(11.5, 15, 0.5, 1),
+		'90_days' 	: genCombos(11, 15, 0.5, 1)
 	}
 }	
 
