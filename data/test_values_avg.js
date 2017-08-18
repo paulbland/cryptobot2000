99ca9f2b236739dcad1cbda74d27000271e86f98
Paul Bland <paul.bland@gmail.com>
2017-08-18 08:53:56 -0400

    sim vars

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -2,25 +2,25 @@ module.exports = {
 	
 	// EVERYTHING FROM SIM VARs +/- 3 units (0.5 or 0.005) - update both every day!
 	low_values : {
-		'30_days' 	: genValues(0.105, 0.140, 0.005, 3), 
+		'30_days' 	: genValues(0.090, 0.135, 0.005, 3), 
 		'45_days' 	: genValues(0.145, 0.180, 0.005, 3), 
 		'60_days' 	: genValues(0.145, 0.185, 0.005, 3), 
 		'75_days' 	: genValues(0.145, 0.190, 0.005, 3),
-		'90_days' 	: genValues(0.135, 0.175, 0.005, 3)
+		'90_days' 	: genValues(0.130, 0.175, 0.005, 3)
 	},
 	high_values : {
-		'30_days' 	: genValues(0.155, 0.210, 0.005, 3),  
+		'30_days' 	: genValues(0.155, 0.220, 0.005, 3),  
 		'45_days' 	: genValues(0.170, 0.240, 0.005, 3),  
 		'60_days' 	: genValues(0.170, 0.225, 0.005, 3), 
 		'75_days' 	: genValues(0.185, 0.240, 0.005, 3), 
 		'90_days' 	: genValues(0.190, 0.230, 0.005, 3)
 	},
 	period_offset : {
-		'30_days' 	: genCombos(17.5, 21.5, 0.5, 1),
+		'30_days' 	: genCombos(18.5, 23, 0.5, 1),
 		'45_days' 	: genCombos(16.5, 20.5, 0.5, 1),  
 		'60_days' 	: genCombos(16, 20.5, 0.5, 1),  
 		'75_days' 	: genCombos(15.5, 20, 0.5, 1),
-		'90_days' 	: genCombos(11, 15.5, 0.5, 1)
+		'90_days' 	: genCombos(10, 15, 0.5, 1)
 	}
 }	
 
