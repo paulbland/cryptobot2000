4da4fab453328db7b1173a5e817ff01a926a95ce
Paul Bland <paul.bland@gmail.com>
2017-12-01 16:26:42 -0500

    shutting down

--- clock.js
+++ clock.js
@@ -5,8 +5,8 @@ var config 		        = require('./config/config')
 
 new CronJob('*/5 * * * *', function()       { priceBot.run() }, null, true, config.timezone);               // Every 5 min starting at hh:00:00
 
-new CronJob('1-59/5 * * * *', function()    { liveBot.run('30_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:01:00
-new CronJob('30 1-59/5 * * * *', function() { liveBot.run('45_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:01:30
-new CronJob('2-59/5 * * * *', function()    { liveBot.run('60_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:02:00
-new CronJob('30 2-59/5 * * * *', function() { liveBot.run('75_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:02:30
-new CronJob('3-59/5 * * * *', function()    { liveBot.run('90_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:03:00
+// new CronJob('1-59/5 * * * *', function()    { liveBot.run('30_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:01:00
+// new CronJob('30 1-59/5 * * * *', function() { liveBot.run('45_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:01:30
+// new CronJob('2-59/5 * * * *', function()    { liveBot.run('60_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:02:00
+// new CronJob('30 2-59/5 * * * *', function() { liveBot.run('75_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:02:30
+// new CronJob('3-59/5 * * * *', function()    { liveBot.run('90_day_bot') }, null, true, config.timezone);   // Every 5 min starting at hh:03:00
