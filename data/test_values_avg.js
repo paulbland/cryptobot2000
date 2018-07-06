e89a74bf5ff2c5c9fedf2ecc1545204a4677dc51
Paul Bland <paul.bland@gmail.com>
2018-07-06 00:09:21 -0400

    sim vars

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -5,31 +5,31 @@ module.exports = {
 	// max since aug 16 (since turned off crash)  +/- 1 unit 
 	// precision was originally 0.005 / 0.5
 	low_values : {
-		'30_days' 	: genValues(0.085, 0.135, 0.005, 3), // // 0:48
-		'60_days' 	: genValues(0.115, 0.125, 0.005, 3), // // 0:13   
-		'90_days' 	: genValues(0.025, 0.045, 0.005, 3), // // 0:02  
-		'120_days' 	: genValues(0.120, 0.130, 0.005, 3), // // 0:06 
-		'180_days' 	: genValues(0.195, 0.230, 0.005, 3), // // 0:35
-		'270_days' 	: genValues(0.025, 0.050, 0.005, 3), // // 0:47 
-		'360_days' 	: genValues(0.120, 0.155, 0.005, 3)  // // 0:51
+		'30_days' 	: genValues(0.080, 0.130, 0.005, 3), // // 0:48
+		'60_days' 	: genValues(0.115, 0.130, 0.005, 3), // // 0:13   
+		'90_days' 	: genValues(0.020, 0.045, 0.005, 3), // // 0:02  
+		'120_days' 	: genValues(0.115, 0.130, 0.005, 3), // // 0:06 
+		'180_days' 	: genValues(0.205, 0.230, 0.005, 3), // // 0:35
+		'270_days' 	: genValues(0.030, 0.055, 0.005, 3), // // 0:47 
+		'360_days' 	: genValues(0.150, 0.160, 0.005, 3)  // // 0:51
 	},
 	high_values : { 
-		'30_days' 	: genValues(0.010, 0.045, 0.005, 3),  // 
-		'60_days' 	: genValues(0.000, 0.045, 0.005, 3),  // 
-		'90_days' 	: genValues(0.025, 0.050, 0.005, 3),  // 
-		'120_days' 	: genValues(0.095, 0.105, 0.005, 3),  // 
-		'180_days' 	: genValues(0.020, 0.080, 0.005, 3),  // 
-		'270_days' 	: genValues(0.205, 0.275, 0.005, 3),  // 
-		'360_days' 	: genValues(0.220, 0.305, 0.005, 3)   // 
+		'30_days' 	: genValues(0.015, 0.045, 0.005, 3),  // 
+		'60_days' 	: genValues(0.005, 0.050, 0.005, 3),  // 
+		'90_days' 	: genValues(0.025, 0.045, 0.005, 3),  // 
+		'120_days' 	: genValues(0.090, 0.110, 0.005, 3),  // 
+		'180_days' 	: genValues(0.040, 0.070, 0.005, 3),  // 
+		'270_days' 	: genValues(0.210, 0.280, 0.005, 3),  // 
+		'360_days' 	: genValues(0.250, 0.310, 0.005, 3)   // 
 	},
 	period_offset : {
 		'30_days' 	: genCombos(17.5, 36.5, 0.5, 1),  	// 
-		'60_days' 	: genCombos(35.5, 40.5, 0.5, 1),  	//  
-		'90_days' 	: genCombos(4.5, 8.5, 0.5, 1),   	// 
-		'120_days' 	: genCombos(24.5, 30.5, 0.5, 1),  	//  
-		'180_days' 	: genCombos(14.5, 17.5, 0.5, 1), 	//  
-		'270_days' 	: genCombos(8.5, 13.5, 0.5, 1),		//  
-		'360_days' 	: genCombos(7.5, 10.5, 0.5, 1)   	// 
+		'60_days' 	: genCombos(35, 39.5, 0.5, 1),  	//  
+		'90_days' 	: genCombos(4, 6.5, 0.5, 1),   	// 
+		'120_days' 	: genCombos(25, 30, 0.5, 1),  	//  
+		'180_days' 	: genCombos(15, 16.5, 0.5, 1), 	//  
+		'270_days' 	: genCombos(8.5, 11.5, 0.5, 1),		//  
+		'360_days' 	: genCombos(8.5, 10.5, 0.5, 1)   	// 
 	}
 }	
 
