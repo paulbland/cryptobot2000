59b570ac4948257f4d3dd4478f874b97c73b8a72
Paul Bland <paul.bland@gmail.com>
2018-08-19 15:55:09 -0400

    cancel everything for a bit

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -1,7 +1,7 @@
 module.exports = {
 	
 	// 			period	offset	(sum)	low		high	value
-	// 15 --- 	1		25		(26)	0.130	0.000	$198.86		- r2 - running
+	// 15 --- 	1		25		(26)	0.130	0.000	$198.86		- running 
 	// 30 --- 	1		25		(26)	0.130	0.000	$197.97
 	// 45 --- 	1		25		(26)	0.130	0.000	$314.08
 	// 60 --- 	1		25		(26)	0.130	0.030	$389.79
@@ -16,10 +16,12 @@ module.exports = {
 
 	// all +/- 3 units from last sim vars1
 
-	// genValues(0.000, 0.250, 0.010, 3), 15->105
-	// genValues(0.000, 0.250, 0.020, 3), 120-> 
+	// 15->105
+	// genValues(0.000, 0.250, 0.010, 3), 
+	// 120-> 
+	// genValues(0.000, 0.250, 0.020, 3), 
 	low_values : {
-		'15_days' 	: genValues(0.115, 0.145, 0.005, 3), 
+		'15_days' 	: genValues(0.000, 0.250, 0.010, 3),  // genValues(0.115, 0.145, 0.005, 3), 
 		'30_days' 	: genValues(0.115, 0.145, 0.005, 3), 
 		'45_days' 	: genValues(0.115, 0.145, 0.005, 3), 
 		'60_days' 	: genValues(0.115, 0.145, 0.005, 3), 
@@ -33,33 +35,34 @@ module.exports = {
 		'180_days' 	: genValues(0.145, 0.175, 0.005, 3), 
 	},
 	high_values : { 
-		'15_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'30_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'45_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'60_days' 	: genValues(0.015, 0.045, 0.005, 3), // done
-		'75_days' 	: genValues(0.000, 0.025, 0.005, 3), // done  
-		'90_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'105_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'120_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'135_days' 	: genValues(0.065, 0.095, 0.005, 3), // done
-		'150_days' 	: genValues(0.225, 0.255, 0.005, 3), // done    
-		'165_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
-		'180_days' 	: genValues(0.000, 0.015, 0.005, 3), // done
+		'15_days' 	: genValues(0.000, 0.250, 0.010, 3),  // genValues(0.000, 0.015, 0.005, 3),
+		'30_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'45_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'60_days' 	: genValues(0.015, 0.045, 0.005, 3), 
+		'75_days' 	: genValues(0.000, 0.025, 0.005, 3), 
+		'90_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'105_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'120_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'135_days' 	: genValues(0.065, 0.095, 0.005, 3), 
+		'150_days' 	: genValues(0.225, 0.255, 0.005, 3), 
+		'165_days' 	: genValues(0.000, 0.015, 0.005, 3), 
+		'180_days' 	: genValues(0.000, 0.015, 0.005, 3), 
 	},
-	// genCombos(2, 48, 2, 1), 15->
+	// 15->
+	// genCombos(2, 48, 2, 1), 
 	period_offset : {
-		'15_days' 	: genCombos(23, 29, 1, 1), // done
-		'30_days' 	: genCombos(23, 29, 1, 1), // done
-		'45_days' 	: genCombos(23, 29, 1, 1), // done
-		'60_days' 	: genCombos(23, 29, 1, 1), // done
-		'75_days' 	: genCombos(31, 37, 1, 1), // done
-		'90_days' 	: genCombos(37, 43, 1, 1), // done
-		'105_days' 	: genCombos(37, 43, 1, 1), // done
-		'120_days' 	: genCombos(37, 43, 1, 1), // done
-		'135_days' 	: genCombos(9, 15, 1, 1),  // done
-		'150_days' 	: genCombos(35, 41, 1, 1), // done
-		'165_days' 	: genCombos(41, 47, 1, 1), // done
-		'180_days' 	: genCombos(41, 47, 1, 1), // done
+		'15_days' 	: genCombos(2, 48, 2, 1),  // genCombos(23, 29, 1, 1), 
+		'30_days' 	: genCombos(23, 29, 1, 1), 
+		'45_days' 	: genCombos(23, 29, 1, 1), 
+		'60_days' 	: genCombos(23, 29, 1, 1), 
+		'75_days' 	: genCombos(31, 37, 1, 1), 
+		'90_days' 	: genCombos(37, 43, 1, 1), 
+		'105_days' 	: genCombos(37, 43, 1, 1), 
+		'120_days' 	: genCombos(37, 43, 1, 1), 
+		'135_days' 	: genCombos(9, 15, 1, 1),  
+		'150_days' 	: genCombos(35, 41, 1, 1), 
+		'165_days' 	: genCombos(41, 47, 1, 1), 
+		'180_days' 	: genCombos(41, 47, 1, 1), 
 	}
 }	
 
