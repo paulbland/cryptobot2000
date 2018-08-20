c5c510f94fc055fe20ab3f7b89d5f96220d0dbdb
Paul Bland <paul.bland@gmail.com>
2018-08-20 18:11:58 -0400

    debugging

--- bots/live-bot.js
+++ bots/live-bot.js
@@ -145,7 +145,7 @@ module.exports = {
 		var latest_buy_price 	= price_data[this_index].value_buy;									// this will be the currect price we're evaluating
 		var latest_sell_price 	= price_data[this_index].value_sell;								// this will be the currect price we're evaluating
 		var avg_for_period 		= tools.calculateAverage(data_to_be_tested) 
-
+console.log(avg_for_period)
 
 		// decide buy or sell
 		var sell_or_buy = tools.decideBuyOrSell(data_to_be_tested, latest_buy_price, latest_sell_price, this.low_threshold, this.high_threshold, buy_sell_method, print_full_debug, false)
