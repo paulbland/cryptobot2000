406aba21982e36818e8439ca308762b4ff4bc82c
Paul Bland <paul.bland@gmail.com>
2018-08-19 21:10:57 -0400

    pausing clock

--- bots/live-bot.js
+++ bots/live-bot.js
@@ -326,7 +326,7 @@ module.exports = {
 			if (err) {
 				console.log(err);
 			}
-			self.debug(`Finished.`);// (Saved newLiveData (ETH) record)`);
+			// self.debug(`Finished.`);// (Saved newLiveData (ETH) record)`);
 			self.running = false; //return true;
 		})
 
