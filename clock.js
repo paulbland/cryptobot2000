16bd85e39bb011952c12438d4f8ec4c3d87995b7
Paul Bland <paul.bland@gmail.com>
2018-08-26 22:40:26 -0400

    lets see what happens to mem when we run every min

--- clock.js
+++ clock.js
@@ -11,10 +11,18 @@ var config 		        = require('./config/config')
 
 new CronJob('0 * * * *', function() { priceBot.run() }, null, true, config.timezone); // every hour at hh:00
 
-// 5 so it doesnt run at exact same time as price bot (running on 00)
-new CronJob('1-59/5 * * * *', function() { liveBot.run('30_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:00
-new CronJob('30 1-59/5 * * * *', function() { liveBot.run('60_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:30
-new CronJob('2-59/5 * * * *', function() { liveBot.run('90_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:00
-new CronJob('30 2-59/5 * * * *', function() { liveBot.run('120_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:30
-new CronJob('3-59/5 * * * *', function() { liveBot.run('150_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:00
-new CronJob('30 3-59/5 * * * *', function() { liveBot.run('180_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:30
+// every minute!
+new CronJob('5 * * * * *', function() { liveBot.run('30_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:00
+new CronJob('15 * * * * *', function() { liveBot.run('60_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:30
+new CronJob('25 * * * * *', function() { liveBot.run('90_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:00
+new CronJob('35 * * * * *', function() { liveBot.run('120_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:30
+new CronJob('45 * * * * *', function() { liveBot.run('150_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:00
+new CronJob('55 * * * *', function() { liveBot.run('180_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:30
+
+// new CronJob('1-59/5 * * * *', function() { liveBot.run('30_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:00
+// new CronJob('30 1-59/5 * * * *', function() { liveBot.run('60_day_bot') }, null, true, config.timezone); // every 5 min at hh:01:30
+// new CronJob('2-59/5 * * * *', function() { liveBot.run('90_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:00
+// new CronJob('30 2-59/5 * * * *', function() { liveBot.run('120_day_bot') }, null, true, config.timezone); // every 5 min at hh:02:30
+// new CronJob('3-59/5 * * * *', function() { liveBot.run('150_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:00
+// new CronJob('30 3-59/5 * * * *', function() { liveBot.run('180_day_bot') }, null, true, config.timezone); // every 5 min at hh:03:30
+
