854b6b9963d12ae6120fe8e5c786271897dd24e2
Paul Bland <paul.bland@gmail.com>
2018-09-27 19:12:10 -0400

    just go back where we were

--- data/test_values_avg.js
+++ data/test_values_avg.js
@@ -22,12 +22,12 @@ module.exports = {
 		'180_days' 	: genValues(0.000, 0.220, 0.015, 3),
 	},
 	period_offset : {
-		'30_days' 	: genCombos(22, 392, 4, 1),
-		'60_days' 	: genCombos(22, 392, 4, 1),
-		'90_days' 	: genCombos(22, 392, 4, 1),
-		'120_days' 	: genCombos(22, 392, 4, 1),
-		'150_days' 	: genCombos(22, 392, 4, 1),
-		'180_days' 	: genCombos(22, 392, 4, 1),
+		'30_days' 	: genCombos(22, 262, 4, 1),
+		'60_days' 	: genCombos(22, 262, 4, 1),
+		'90_days' 	: genCombos(22, 262, 4, 1),
+		'120_days' 	: genCombos(22, 262, 4, 1),
+		'150_days' 	: genCombos(22, 262, 4, 1),
+		'180_days' 	: genCombos(22, 262, 4, 1),
 	}
 }
 
